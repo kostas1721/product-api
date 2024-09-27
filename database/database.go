28d06f900270246b0962961a2aa3121b8334864c
kostas1721 <konstantinos1721@gmail.com>
2024-09-27 17:27:33 +0300

    new db

--- database/database.go
+++ database/database.go
@@ -9,7 +9,7 @@ var DB *sql.DB
 
 func InitDB() {
     var err error
-    DB, err = sql.Open("sqlite3", "./products.db")
+    DB, err = sql.Open("sqlite", "./products.db")
     if err != nil {
         panic(err)
     }
